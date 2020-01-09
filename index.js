2d4e4831620525614278dcc8757ccc61076426ac
korpozim <korpozim@gmail.com>
2020-01-09 20:04:25 +0300

    add new methods

--- index.js
+++ index.js
@@ -9,5 +9,59 @@ module.exports = {
 
     wordsfirstup: function (text) {
         return text.toString().split(" ").map(x => x[0].toUpperCase() + x.substring(1, x.length)).join(" ");
+    },
+
+    upper: function (text) {
+        return text.toString().toUpperCase();
+    },
+
+    lower: function (text) {
+        return text.toString().toLowerCase();
+    },
+
+    doublequotes: function (text) {
+        return "\"" + text.toString() + "\"";
+    },
+
+    singlequotes: function (text) {
+        return "\'" + text.toString() + "\'";
+    },
+
+    brackets: function (text) {
+        return "(" + text.toString() + ")";
+    },
+
+    blabla: function (text) {
+        return text.toString() + "...";
+    },
+
+    camelcase: function (text) {
+        return text.toString().split(" ").map((word, index) => {
+            let tempWord = word.toString().toLowerCase();
+
+            if (index != 0) {
+                tempWord = tempWord[0].toUpperCase() + tempWord.substring(1, tempWord.length)
+            }
+
+            return tempWord;
+        }).join("");
+    },
+
+    pascalcase: function (text) {
+        return text.toString().split(" ").map((word) => {
+            return word.toString()[0].toUpperCase() + word.toString().toLowerCase().substring(1, word.toString().length);
+        }).join("");
+    },
+
+    snakecase: function (text) {
+        return text.toString().split(" ").map(x => x.toString().toLowerCase()).join("_");
+    },
+
+    kebabcase: function (text) {
+        return text.toString().split(" ").map(x => x.toString().toLowerCase()).join("-");
+    },
+
+    trim: function (text) {
+        return text.toString().trim();
     }
 }
--- spec/appSpec.js
+++ spec/appSpec.js
@@ -20,4 +20,59 @@ describe("App", function () {
         expect(helper.wordsfirstup("Test Test")).toEqual("Test Test");
     });
 
+    it("upper test", function () {
+        expect(helper.upper("test")).toEqual("TEST");
+        expect(helper.upper("test test")).toEqual("TEST TEST");
+    });
+
+    it("lower test", function () {
+        expect(helper.lower("TEST")).toEqual("test");
+        expect(helper.lower("TEST TEST")).toEqual("test test");
+    });
+
+    it("doublequotes test", function () {
+        expect(helper.doublequotes("test")).toEqual("\"test\"");
+    });
+
+    it("singlequotes test", function () {
+        expect(helper.singlequotes("test")).toEqual("\'test\'");
+    });
+
+    it("brackets test", function () {
+        expect(helper.brackets("test")).toEqual("(test)");
+    });
+
+    it("blabla test", function () {
+        expect(helper.blabla("test")).toEqual("test...");
+    });
+
+    it("camelcase test", function () {
+        expect(helper.camelcase("test")).toEqual("test");
+        expect(helper.camelcase("test test")).toEqual("testTest");
+    });
+
+    it("pascalcase test", function () {
+        expect(helper.pascalcase("test")).toEqual("Test");
+        expect(helper.pascalcase("test test")).toEqual("TestTest");
+    });
+
+    it("snakecase test", function () {
+        expect(helper.snakecase("test")).toEqual("test");
+        expect(helper.snakecase("TEST")).toEqual("test");
+        expect(helper.snakecase("test test")).toEqual("test_test");
+    });
+
+    it("kebabcase test", function () {
+        expect(helper.kebabcase("test")).toEqual("test");
+        expect(helper.kebabcase("TEST")).toEqual("test");
+        expect(helper.kebabcase("test test")).toEqual("test-test");
+    });
+
+    it("trim test", function () {
+        expect(helper.trim("test")).toEqual("test");
+        expect(helper.trim(" test")).toEqual("test");
+        expect(helper.trim("test ")).toEqual("test");
+        expect(helper.trim(" test ")).toEqual("test");
+    });
+
 });  
